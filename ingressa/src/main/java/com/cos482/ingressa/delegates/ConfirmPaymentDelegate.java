0e022823086ee6897d4662ebca2a08041e9348d0
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-06-08 02:00:48 -0300

    Add delegate

---
+++ ingressa/src/main/java/com/cos482/ingressa/delegates/ConfirmPaymentDelegate.java
@@ -0,0 +1,23 @@
+package java.com.cos482.delegates;
+
+import com.cos482.ingressa.domain.MovieTicket;
+import com.cos482.ingressa.service.dto.MovieTicketProcessDTO;
+import java.math.BigDecimal;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfirmPaymentDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        MovieTicketProcessDTO pi = (MovieTicketProcessDTO) delegateExecution.getVariable("pi");
+        String name = pi.getMovieTicket().getName();
+        System.out.println("=================================================");
+        System.out.println("=============== CARD VALIDATED ======================");
+        System.out.println("=============== " + name + "======================");
+        System.out.println("=================================================");
+    }
+}
